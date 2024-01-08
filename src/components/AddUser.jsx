0983cd713cbb4460e05abcab40c3fed64a79a26f
kuzhalikumaraguru <kuzhali.kumaraguru@gmail.com>
2024-01-08 20:21:02 +0530

    formik for form validation changes committed

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -4,21 +4,49 @@ import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import ApiService from "../utils/ApiService";
 import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 function AddUser() {
-  let [name, setName] = useState("");
-  let [email, setEmail] = useState("");
-  let [batch, setBatch] = useState("");
-  let [mobile, setMobile] = useState("");
   let navigate = useNavigate();
-  let handleCreate = async () => {
-    let data = { name, email, batch, mobile };
-    let res = await ApiService.post("/users", data);
-    if (res.status === 201) {
-      // toast.success("User Added Successfully")
-      navigate("/dashboard");
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      mobile: '',
+      batch: ''
+    },
+    validationSchema: Yup.object({
+      name:   Yup.string().required('Name is Required').max(20, 'Not exceed more than 20 characters').min(3, 'Atleast 3 characters'),
+      email:  Yup.string().required('Email is Required').email('Email is not valid'),
+      mobile: Yup.string().required('Mobile is Required').matches(/^\d{10}$/, 'Enter 10 digits') ,
+      batch:  Yup.string().required('Batch is Required')
+    }),
+    onSubmit: async (values) => {
+      console.log(values);
+      try {
+        let res = await ApiService.post('/users', values);
+        if (res.status === 201) {
+          navigate('/dashboard');
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
-  };
+  });
+  // let [name, setName] = useState("");
+  // let [email, setEmail] = useState("");
+  // let [batch, setBatch] = useState("");
+  // let [mobile, setMobile] = useState("");
+
+  // let handleCreate = async () => {
+  //   let data = { name, email, batch, mobile };
+  //   let res = await ApiService.post("/users", data);
+  //   if (res.status === 201) {
+  //     // toast.success("User Added Successfully")
+  //     navigate("/dashboard");
+  //   }
+  // };
   return (
     <div id="content-wrapper" className="d-flex flex-column">
       <div id="content">
@@ -26,46 +54,52 @@ function AddUser() {
           <div className="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 className="h3 mb-0 text-gray-800">Add User</h1>
           </div>
-          <Form>
+          <Form onSubmit={formik.handleSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Name"
-                onChange={(e) => setName(e.target.value)}
+                placeholder="Name" id="name" name="name"
+                onChange={formik.handleChange}
+                value={formik.values.name}  onBlur={formik.handleBlur}
               />
+              {formik.touched.name && formik.errors.name ? (<div style={{color:"red"}}>{formik.errors.name}</div>) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
-                placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter email" id="email" name="email"
+                onChange={formik.handleChange}
+                value={formik.values.email} onBlur={formik.handleBlur}
               />
+              {formik.touched.email && formik.errors.email ? (<div style={{color:"red"}}>{formik.errors.email}</div>) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Mobile</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Mobile"
-                onChange={(e) => setMobile(e.target.value)}
+                placeholder="Mobile" id="mobile" name="mobile"
+                onChange={formik.handleChange}
+                value={formik.values.mobile} onBlur={formik.handleBlur}
               />
+              {formik.touched.mobile && formik.errors.mobile ? (<div style={{color:"red"}}>{formik.errors.mobile}</div>) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Batch</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Batch"
-                onChange={(e) => setBatch(e.target.value)}
+                placeholder="Batch" id="batch" name="batch"
+                onChange={formik.handleChange}
+                value={formik.values.batch} onBlur={formik.handleBlur}
               />
+              {formik.touched.batch && formik.errors.batch ? (<div style={{color:"red"}}>{formik.errors.batch}</div>) : null}
             </Form.Group>
 
-            <Button variant="primary" onClick={() => handleCreate()}>
-              Submit
-            </Button>
+            <Button variant="primary" type="submit">Submit</Button>
           </Form>
         </div>
       </div>
--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -4,40 +4,73 @@ import Button from "react-bootstrap/Button";
 import { useNavigate, useParams } from "react-router-dom";
 import ApiService from "../utils/ApiService";
 import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 function EditUser() {
-  let [name, setName] = useState("");
-  let [email, setEmail] = useState("");
-  let [batch, setBatch] = useState("");
-  let [mobile, setMobile] = useState("");
+  // let [name, setName] = useState("");
+  // let [email, setEmail] = useState("");
+  // let [batch, setBatch] = useState("");
+  // let [mobile, setMobile] = useState("");
+  let [initialValues, setValues] = useState({
+    name: '',
+    email: '',
+    mobile: '',
+    batch: ''
+  });
   let { id } = useParams();
   let navigate = useNavigate();
-  let handleEdit = async () => {
-    try {
-      let res = await ApiService.put(`/users/${id}`, {
-        id,
-        name,
-        email,
-        batch,
-        mobile,
-      });
-      if (res.status === 200) {
-        // toast.success("User Updated Successfully")
-        navigate("/dashboard");
-      }
-    } catch (error) {}
-  };
   let getData = async () => {
     try {
       let res = await ApiService.get(`/users/${id}`);
       if (res.status === 200) {
-        setName(res.data.name);
-        setMobile(res.data.mobile);
-        setBatch(res.data.batch);
-        setEmail(res.data.email);
+        setValues({
+          name: res.data.name,
+          email: res.data.email,
+          mobile: res.data.mobile,
+          batch: res.data.batch,
+        });
+        // setName(res.data.name);
+        // setMobile(res.data.mobile);
+        // setBatch(res.data.batch);
+        // setEmail(res.data.email);
       }
     } catch (error) {}
   };
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: Yup.object({
+      name:   Yup.string().required('Name is Required').max(20, 'Not exceed more than 20 characters').min(3, 'Atleast 3 characters'),
+      email:  Yup.string().required('Email is Required').email('Email is not valid'),
+      mobile: Yup.string().required('Mobile is Required').matches(/^\d{10}$/, 'Enter 10 digits') ,
+      batch:  Yup.string().required('Batch is Required')
+    }),
+    enableReinitialize: true, //to reinitialize or reload the form to change then values
+    onSubmit: async (values) => {
+      values.id = id;
+      try {
+        let res = await ApiService.put(`/users/${id}`, values);
+        if (res.status === 200) {
+          navigate("/dashboard");
+        }
+      } catch (error) {}
+    },
+  });
+  // let handleEdit = async () => {
+  //   try {
+  //     let res = await ApiService.put(`/users/${id}`, {
+  //       id,
+  //       name,
+  //       email,
+  //       batch,
+  //       mobile,
+  //     });
+  //     if (res.status === 200) {
+  //       // toast.success("User Updated Successfully")
+  //       navigate("/dashboard");
+  //     }
+  //   } catch (error) {}
+  // };
 
   useEffect(() => {
     getData();
@@ -50,15 +83,21 @@ function EditUser() {
           <div className="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 className="h3 mb-0 text-gray-800">Edit User</h1>
           </div>
-          <Form>
+          <Form onSubmit={formik.handleSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                id="name"
+                name="name"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                onBlur={formik.handleBlur}
               />
+              {formik.touched.name && formik.errors.name ? (
+                <div style={{ color: "red" }}>{formik.errors.name}</div>
+              ) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -66,9 +105,15 @@ function EditUser() {
               <Form.Control
                 type="email"
                 placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                name="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
               />
+              {formik.touched.email && formik.errors.email ? (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+              ) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -76,9 +121,15 @@ function EditUser() {
               <Form.Control
                 type="text"
                 placeholder="Mobile"
-                value={mobile}
-                onChange={(e) => setMobile(e.target.value)}
+                id="mobile"
+                name="mobile"
+                onChange={formik.handleChange}
+                value={formik.values.mobile}
+                onBlur={formik.handleBlur}
               />
+              {formik.touched.mobile && formik.errors.mobile ? (
+                <div style={{ color: "red" }}>{formik.errors.mobile}</div>
+              ) : null}
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -86,13 +137,19 @@ function EditUser() {
               <Form.Control
                 type="text"
                 placeholder="Batch"
-                value={batch}
-                onChange={(e) => setBatch(e.target.value)}
+                id="batch"
+                name="batch"
+                onChange={formik.handleChange}
+                value={formik.values.batch}
+                onBlur={formik.handleBlur}
               />
+              {formik.touched.batch && formik.errors.batch ? (
+                <div style={{ color: "red" }}>{formik.errors.batch}</div>
+              ) : null}
             </Form.Group>
 
-            <Button variant="primary" onClick={() => handleEdit()}>
-              Submit
+            <Button variant="primary" type="submit">
+              Update
             </Button>
           </Form>
         </div>
